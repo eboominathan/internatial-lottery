ab9771b9c0d5592f313327233876d4f22c520528
Boominathan <eboominathan@gmail.com>
2023-01-17 20:42:05 +0700

    index updated

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\LotteryController;
 | Web Routes
 |--------------------------------------------------------------------------
 */
+Route::get('/', [CustomAuthController::class, 'index'])->name('login'); 
 Route::get('login', [CustomAuthController::class, 'index'])->name('login'); 
 Route::post('login', [CustomAuthController::class, 'customLogin'])->name('custom login'); 
 Route::get('register', [CustomAuthController::class, 'registration'])->name('register'); 
